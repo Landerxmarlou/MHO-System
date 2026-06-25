55eff6b8a0f48e20006c24d7ada545086bc759ab
Landerxmarlou <jhonllanderpangangaan@gmail.com>
2026-06-25 08:06:16 +0800

    Delete database directory

--- database/migrations/001_add_profile_image.sql
+++
@@ -1,5 +0,0 @@
--- Add profile image support for users
-ALTER TABLE users
-    ADD COLUMN profile_image VARCHAR(255) NULL DEFAULT NULL
-    COMMENT 'Relative path to uploaded profile photo'
-    AFTER phone;
