670f30a46fe7fee27c8d99a5293c56df49c58883
Landerxmarlou <jhonllanderpangangaan@gmail.com>
2026-06-25 08:05:16 +0800

    Delete admin directory

--- admin/validate.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once __DIR__ . '/../includes/init.php';
-requireRole(['admin']);
-
-$id = (int) ($_GET['id'] ?? 0);
-$url = roleUrl('admin', 'submissions.php' . ($id ? '?id=' . $id : ''));
-redirect($url);
