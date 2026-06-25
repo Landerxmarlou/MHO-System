c57aa008add15d5b0180686d65e300a45a0a8fcd
Landerxmarlou <jhonllanderpangangaan@gmail.com>
2026-06-25 08:09:18 +0800

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-$userId = (int) ($_SESSION['user_id'] ?? 0);
-
-require_once __DIR__ . '/includes/init.php';
-logout(getDB(), $userId > 0 ? $userId : null);
-redirect(baseUrl('index.php'));
